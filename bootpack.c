e27ee0dc17e41a1e7b8b851be04d142205fbbfce
musou1500 <musou1500@gmail.com>
2019-01-02 04:09:48 +0900

    write_mem8を削除

--- bootpack.c
+++ bootpack.c
@@ -2,8 +2,10 @@ void io_hlt(void);
 void write_mem8(int addr, int data);
 
 void vidar_main(void) {
+  char *p;
   for (int i = 0xa0000; i <= 0xaffff; i++) {
-    write_mem8(i, 15 & i);
+    p = (char *)i;
+    *p = i & 0x0f;
   }
 
   for(;;) {
--- func.s
+++ func.s
@@ -6,8 +6,3 @@
 io_hlt:
   hlt
   ret
-write_mem8:
-  mov ecx,[esp + 4]
-  mov al,[esp + 8]
-  mov [ecx],al
-  ret
