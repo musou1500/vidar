8fb2cff2413d6c3f4c46fcbbb565b64c40504434
musou1500 <musou1500@gmail.com>
2019-01-03 09:01:49 +0900

    putfont8を実装

--- bootpack.c
+++ bootpack.c
@@ -25,20 +25,26 @@ typedef struct {
 } BootInfo;
 
 void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0,
               int x1, int y1);
 void init_screen(char *vram, int xsize, int ysize);
 
+
+
 void vidar_main(void) {
   BootInfo *binfo = (BootInfo *)0x0ff0;
   char *vram = binfo->vram;
-
+  static char font_A[16] = {
+    0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
+    0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
+  };
   init_palette();
 
   int xsize = binfo->scrnx;
   int ysize = binfo->scrny;
   init_screen(vram, xsize, ysize);
+  putfont8(binfo->vram, binfo->scrnx, 10, 10, COL_WHITE, font_A);
 
   for (;;) {
     io_hlt();
@@ -66,28 +72,28 @@ void init_screen(char *vram, int xsize, int ysize) {
   boxfill8(vram, xsize, COL_WHITE, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
 }
 
-void init_palette(void) {
-  // staticにすると，色が正しく表示されないので，仕方なくローカル変数を使っている．
-  // ブートのときに意図しない領域に書き込みを行っているのかもしれない
-  static unsigned char table_rgb[] = {
-      0x00, 0x00, 0x00, /*  0:黒 */
-      0xff, 0x00, 0x00, /*  1:明るい赤 */
-      0x00, 0xff, 0x00, /*  2:明るい緑 */
-      0xff, 0xff, 0x00, /*  3:明るい黄色 */
-      0x00, 0x00, 0xff, /*  4:明るい青 */
-      0xff, 0x00, 0xff, /*  5:明るい紫 */
-      0x00, 0xff, 0xff, /*  6:明るい水色 */
-      0xff, 0xff, 0xff, /*  7:白 */
-      0xc6, 0xc6, 0xc6, /*  8:明るい灰色 */
-      0x84, 0x00, 0x00, /*  9:暗い赤 */
-      0x00, 0x84, 0x00, /* 10:暗い緑 */
-      0x84, 0x84, 0x00, /* 11:暗い黄色 */
-      0x00, 0x00, 0x84, /* 12:暗い青 */
-      0x84, 0x00, 0x84, /* 13:暗い紫 */
-      0x00, 0x84, 0x84, /* 14:暗い水色 */
-      0x84, 0x84, 0x84  /* 15:暗い灰色 */
-  };
+// staticにすると，色が正しく表示されないので，仕方なくローカル変数を使っている．
+// ブートのときに意図しない領域に書き込みを行っているのかもしれない
+static unsigned char table_rgb[] = {
+  0x00, 0x00, 0x00, /*  0:黒 */
+  0xff, 0x00, 0x00, /*  1:明るい赤 */
+  0x00, 0xff, 0x00, /*  2:明るい緑 */
+  0xff, 0xff, 0x00, /*  3:明るい黄色 */
+  0x00, 0x00, 0xff, /*  4:明るい青 */
+  0xff, 0x00, 0xff, /*  5:明るい紫 */
+  0x00, 0xff, 0xff, /*  6:明るい水色 */
+  0xff, 0xff, 0xff, /*  7:白 */
+  0xc6, 0xc6, 0xc6, /*  8:明るい灰色 */
+  0x84, 0x00, 0x00, /*  9:暗い赤 */
+  0x00, 0x84, 0x00, /* 10:暗い緑 */
+  0x84, 0x84, 0x00, /* 11:暗い黄色 */
+  0x00, 0x00, 0x84, /* 12:暗い青 */
+  0x84, 0x00, 0x84, /* 13:暗い紫 */
+  0x00, 0x84, 0x84, /* 14:暗い水色 */
+  0x84, 0x84, 0x84  /* 15:暗い灰色 */
+};
 
+void init_palette(void) {
   int eflags;
   eflags = io_load_eflags();
   io_cli();
@@ -101,8 +107,6 @@ void init_palette(void) {
   return;
 }
 
-void set_palette(int start, int end, unsigned char *rgb) { return; }
-
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0,
               int x1, int y1) {
   for (int y = y0; y <= y1; y++) {
@@ -112,4 +116,23 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0,
   }
 
   return;
+}
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
+{
+	int i;
+	char *p, d /* data */;
+	for (i = 0; i < 16; i++) {
+		p = vram + (y + i) * xsize + x;
+		d = font[i];
+		if ((d & 0x80) != 0) { p[0] = c; }
+		if ((d & 0x40) != 0) { p[1] = c; }
+		if ((d & 0x20) != 0) { p[2] = c; }
+		if ((d & 0x10) != 0) { p[3] = c; }
+		if ((d & 0x08) != 0) { p[4] = c; }
+		if ((d & 0x04) != 0) { p[5] = c; }
+		if ((d & 0x02) != 0) { p[6] = c; }
+		if ((d & 0x01) != 0) { p[7] = c; }
+	}
+	return;
 }
